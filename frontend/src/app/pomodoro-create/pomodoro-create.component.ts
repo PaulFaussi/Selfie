35d1b93c03a3ec1d21b555b65f88ef40d9c8ebd9
PaulFaussi <foka.alessio@gmail.com>
2025-08-27 02:52:36 +0200

    Inizio a creare la creazione del Pomodoro

---
+++ frontend/src/app/pomodoro-create/pomodoro-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pomodoro-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './pomodoro-create.component.html',
+  styleUrl: './pomodoro-create.component.css'
+})
+export class PomodoroCreateComponent {
+
+}
